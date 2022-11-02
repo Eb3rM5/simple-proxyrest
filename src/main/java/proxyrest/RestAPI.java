a9d7e8a34cbd77dfe1feb10ecaa123656d00e291
Éber Mainardes <mainardes.eber@gmail.com>
2022-11-02 15:49:30 -0300

    Matching APIInvocationHandler's instance types to its defined HttpClient

--- src/main/java/proxyrest/RestAPI.java
+++ src/main/java/proxyrest/RestAPI.java
@@ -7,7 +7,7 @@ import proxyrest.client.AbstractHttpClient;
 public interface RestAPI {
 
 	public static <T extends RestAPI> T createAPI(Class<T> apiClass, AbstractHttpClient<?, ?> httpClient) {
-		final Object instance = Proxy.newProxyInstance(RestAPI.class.getClassLoader(), new Class<?>[] { apiClass }, new APIInvocationHandler(httpClient));
+		final Object instance = Proxy.newProxyInstance(RestAPI.class.getClassLoader(), new Class<?>[] { apiClass }, new APIInvocationHandler<>(httpClient));
 		return apiClass.isInstance(instance) ? apiClass.cast(instance) : null;
 	}
 	
