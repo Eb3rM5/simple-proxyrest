b7a7f429deaaf0fa240d6e825e76ef4632f5955a
Éber Mainardes <mainardes.eber@gmail.com>
2023-01-05 22:58:28 -0300

    Adding method to instantiate the API with an ApacheHttpClient by default

--- src/main/java/proxyrest/RestAPI.java
+++ src/main/java/proxyrest/RestAPI.java
@@ -2,10 +2,18 @@ package proxyrest;
 
 import java.lang.reflect.Proxy;
 
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 import proxyrest.client.AbstractHttpClient;
+import proxyrest.client.apache.ApacheHttpClient;
 
 public interface RestAPI {
 
+	public static <T extends RestAPI> T createAPI(Class<T> apiClass) {
+		final ApacheHttpClient apacheHttpClient = new ApacheHttpClient(HttpClients.createDefault());
+		return createAPI(apiClass, apacheHttpClient);
+	}
+	
 	public static <T extends RestAPI> T createAPI(Class<T> apiClass, AbstractHttpClient<?, ?> httpClient) {
 		final Object instance = Proxy.newProxyInstance(RestAPI.class.getClassLoader(), new Class<?>[] { apiClass }, new APIInvocationHandler<>(httpClient));
 		return apiClass.isInstance(instance) ? apiClass.cast(instance) : null;
